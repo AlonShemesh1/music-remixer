308842fbecd08c31524e4ba3fe76abf8cdfc5c50
AlonShemesh1 <alonsh321@gmail.com>
2025-05-12 18:45:10 +0300

    Update audio_processor.py

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -1,72 +1,64 @@
-# utils/audio_processor.py
 from pydub import AudioSegment
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
-import librosa
+import matplotlib.pyplot as plt
+
 
 def load_audio(file):
     return AudioSegment.from_file(file)
 
+
 def save_audio(audio_segment, path):
     audio_segment.export(path, format="mp3")
 
-def detect_chorus(file_path):
-    y, sr = librosa.load(file_path)
-    chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
-    similarity = np.dot(chroma.T, chroma)
-
-    chorus_start = 0
-    chorus_end = 0
-    max_score = 0
-
-    for i in range(similarity.shape[0] - 50):
-        for j in range(i + 30, similarity.shape[1] - 50):
-            score = np.sum(similarity[i:i + 50, j:j + 50])
-            if score > max_score:
-                max_score = score
-                chorus_start = i
-                chorus_end = i + 50
-
-    start_ms = int(librosa.frames_to_time(chorus_start, sr=sr) * 1000)
-    end_ms = int(librosa.frames_to_time(chorus_end, sr=sr) * 1000)
-    return start_ms, end_ms
-
-def mix_with_loops(song, main_loop_path, chorus_loop_path, chorus_start, chorus_end, loop_gain_db=0):
-    main_loop = AudioSegment.from_file(main_loop_path) + loop_gain_db
-    chorus_loop = AudioSegment.from_file(chorus_loop_path) + loop_gain_db
-
-    before_chorus = song[:chorus_start]
-    chorus = song[chorus_start:chorus_end]
-    after_chorus = song[chorus_end:]
-
-    main_looped_before = (main_loop * (len(before_chorus) // len(main_loop) + 1))[:len(before_chorus)]
-    chorus_looped = (chorus_loop * (len(chorus) // len(chorus_loop) + 1))[:len(chorus)]
-    main_looped_after = (main_loop * (len(after_chorus) // len(main_loop) + 1))[:len(after_chorus)]
-
-    mixed = before_chorus.overlay(main_looped_before) + \
-            chorus.overlay(chorus_looped) + \
-            after_chorus.overlay(main_looped_after)
-
-    return mixed
 
 def plot_volume_envelope(audio):
     samples = np.array(audio.get_array_of_samples())
+
     if audio.channels == 2:
         samples = samples.reshape((-1, 2)).mean(axis=1)
 
     window_size = 1000
-    remainder = len(samples) % window_size
-    if remainder != 0:
-        padding = window_size - remainder
-        samples = np.pad(samples, (0, padding), 'constant')
+    num_windows = len(samples) // window_size
 
-    envelope = np.abs(samples).reshape(-1, window_size).mean(axis=1)
-    time = np.linspace(0, len(audio) / 1000, num=len(envelope))
+    if num_windows == 0:
+        st.warning("Audio too short to show volume plot.")
+        return
 
-    fig, ax = plt.subplots()
-    ax.plot(time, envelope)
-    ax.set_xlabel("Time (s)")
-    ax.set_ylabel("Volume")
+    samples = samples[:num_windows * window_size]
+    envelope = np.abs(samples).reshape(num_windows, window_size).mean(axis=1)
+    time = np.linspace(0, len(audio) / 1000, num=num_windows)
+
+    fig, ax = plt.subplots(figsize=(10, 3))
+    ax.plot(time, envelope, color='blue')
     ax.set_title("Volume Envelope")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Amplitude")
     st.pyplot(fig)
+
+
+def apply_loops_with_chorus(song, main_loop_path, chorus_loop_path, loop_gain_db=0):
+    main_loop = AudioSegment.from_file(main_loop_path) + loop_gain_db
+    chorus_loop = AudioSegment.from_file(chorus_loop_path) + loop_gain_db
+
+    song_duration = len(song)
+
+    # Simulated chorus section (from 30s to 45s)
+    chorus_start = 30_000  # ms
+    chorus_end = 45_000    # ms
+
+    part1 = song[:chorus_start]
+    part2 = song[chorus_start:chorus_end]
+    part3 = song[chorus_end:]
+
+    loop1 = main_loop * (len(part1) // len(main_loop) + 1)
+    loop2 = chorus_loop * (len(part2) // len(chorus_loop) + 1)
+    loop3 = main_loop * (len(part3) // len(main_loop) + 1)
+
+    remixed = (
+        part1.overlay(loop1[:len(part1)]) +
+        part2.overlay(loop2[:len(part2)]) +
+        part3.overlay(loop3[:len(part3)])
+    )
+
+    return remixed
