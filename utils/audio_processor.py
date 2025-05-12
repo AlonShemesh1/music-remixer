d6120eeabf889c4b7a2cf2855031030d1c3192fb
AlonShemesh1 <alonsh321@gmail.com>
2025-05-12 17:17:18 +0300

    Update audio_processor.py

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -4,35 +4,24 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import io
-import librosa
 
 
 def load_audio(file):
     return AudioSegment.from_file(file)
 
-def get_bpm(file):
-    y, sr = librosa.load(file, sr=None)
-    tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
-    return int(tempo)
 
-def match_loop_to_bpm(beat, original_bpm, target_bpm):
-    rate = target_bpm / original_bpm
-    beat = beat._spawn(beat.raw_data, overrides={
-        "frame_rate": int(beat.frame_rate * rate)
-    })
-    return beat.set_frame_rate(beat.frame_rate)
-
-def mix_with_beat(song, beat_path, song_bpm, beat_bpm, loop_gain_db=0):
+def mix_with_beat(song, beat_path, loop_gain_db=0):
     beat = AudioSegment.from_file(beat_path)
-    beat = match_loop_to_bpm(beat, beat_bpm, song_bpm)
     beat = beat + loop_gain_db  # Adjust loop volume
     looped_beat = beat * (len(song) // len(beat) + 1)
     looped_beat = looped_beat[:len(song)]
     mixed = song.overlay(looped_beat)
     return mixed
 
+
 def save_audio(audio_segment, path):
     audio_segment.export(path, format="mp3")
+
 
 def plot_volume_envelope(audio):
     samples = np.array(audio.get_array_of_samples())
